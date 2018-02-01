972837eb266dabf74ecb2da9e70f7d894d43cb9f
Matt <du4714@wayne.edu>
2018-01-31 22:19:51 -0500

    location
    location popup working

---
+++ CarpoolApp/Mappagecontroller.swift
@@ -0,0 +1,13 @@
+//
+//  Mappagecontroller.swift
+//  CarpoolApp
+//
+//  Created by Matt on 1/31/18.
+//  Copyright © 2018 CSC 4996. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+import MapKit
--- CarpoolApp/ViewController.swift
+++ CarpoolApp/ViewController.swift
@@ -10,69 +10,63 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    
-    @IBOutlet weak var labelMessage: UILabel!
-    
-    
-    
-    @IBAction func registerButton(_ sender: UIButton)//login information screen
-    {
-        let email = emailField.text
-        let password = passwordField.text
-        
-        Auth.auth().createUser(withEmail: email!, password: password!, completion: { (user: User?, error) in
-            if error == nil {
-                self.labelMessage.text = "Registration Successful!"
-            }else{
-                self.labelMessage.text = "User Already Exists, Please Try Again."
-            }
-            
-        })
-    }
-//trying to implement the function that allows for users location
-    //let locationmanager = CLLocationManager()
-    //override func viewDidLoad(){
-       // super.viewDidLoad()
-    //}
+    @IBOutlet weak var mapview: MKMapView!
+    // Used to start getting the users location
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        FirebaseApp.configure()
-        //locationManager.delegate = self
-        //locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        
+        // For use when the app is open & in the background
+        locationManager.requestAlwaysAuthorization()
+        
+        // For use when the app is open
         //locationManager.requestWhenInUseAuthorization()
-        //locationManager.requestLocation()
+        
+        // If location services is enabled get the users location
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
+            locationManager.startUpdatingLocation()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Print out the location to the console
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location.coordinate)
+        }
     }
-
-
+    
+    // If we have been deined access give the user the option to change it
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisabledPopUp()
+        }
+    }
+    
+    // Show the popup to the user if we have been deined access
+    func showLocationDisabledPopUp() {
+        let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                message: "In order to deliver pizza we need your location",
+                                                preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
-
-//extension ViewController : CLLocationManagerDelegate {
-//    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-//        if status == .AuthorizedWhenInUse {
-//            locationManager.requestLocation()
-//        }
-//}
-//    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        if let location = locations.first {
-//            print("location:: (location)")
-//        }
-//    }
-//    
-//    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-//        print("error:: (error)")
-//    }
-//}
-
