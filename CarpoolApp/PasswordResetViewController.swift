719572035b1dfc3c5a1cbec0541cfbd15ba4bf38
Omer  Khan <okhan320@gmail.com>
2018-01-31 21:02:06 -0500

    Added Password Reset Query.

--- CarpoolApp/PasswordResetViewController.swift
+++ CarpoolApp/PasswordResetViewController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class PasswordResetViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func resetButton(_ sender: UIButton) {
+        
+        
+        let email = self.emailField.text
+        
+        var actionItem = ""
+        var actionTitle = ""
+        let exitAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        if (email?.isEmpty)!
+        {
+            actionTitle = "Error!"
+            actionItem = "The email field is empty."
+            
+            let alert = UIAlertController(title: actionTitle, message: actionItem, preferredStyle: .alert)
+            
+            alert.addAction(exitAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        Auth.auth().sendPasswordReset(withEmail: email!, completion: { (error) in
+            
+            if error != nil {
+                actionTitle = "Error!"
+                actionItem = (error?.localizedDescription)!
+                let alert = UIAlertController(title: actionTitle, message: actionItem, preferredStyle: .alert)
+                
+                alert.addAction(exitAction)
+                
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                actionTitle = "Success!"
+                actionItem = "A link to reset your password has been sent to your email."
+                let alert = UIAlertController(title: actionTitle, message: actionItem, preferredStyle: .alert)
+                
+                alert.addAction(exitAction)
+                
+                self.present(alert, animated: true, completion: nil)
+                self.emailField.text = ""
+            }
+
+            
+
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
