483fed05f53a6d4084bf146256a86aa59cee0b68
Omer  Khan <okhan320@gmail.com>
2018-02-26 16:12:42 -0500

    Integrated RDS into our project, write statements working.

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,16 +6,28 @@ var bodyParser = require('body-parser');
 var firebase = require('firebase');
 var admin = require('firebase-admin');
 var router = express.Router();
-
+var pg = require('pg');
 var db = admin.database();
 var usersRef = db.ref('/Users');
+
+var conString = "postgres://carpool:carpool2018@carpool.clawh88zlo74.us-east-2.rds.amazonaws.com:5432/carpool";
+var client = new pg.Client(conString);
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack);
+  } else {
+    console.log('Connected to DB.');
+  }
+});
+
 
 router.post('/register', function(req, res, next) {
  var userInfo = req.body.userInfo;
  var userJSON = JSON.parse(userInfo);
  var userID = userJSON['userID'];
 
- usersRef.update({
+/* usersRef.update({
   [userID]: {
     lastName:  userJSON['lastName'],
     firstName: userJSON['firstName'],
@@ -23,9 +35,14 @@ router.post('/register', function(req, res, next) {
     email: userJSON['email']
   }
 });
+/*/
+
+client.query("INSERT INTO carpool.\"Users\"(\"userID\", \"firstName\", \"lastName\", \"email\") values($1, $2, $3, $4)", [
+  userID, userJSON['firstName'], userJSON['lastName'], userJSON['email']]);
 
   console.log ("User Set.");
 });
+
 
 
 router.get('/profile', function(req, res, next) {
@@ -33,13 +50,16 @@ var userID = req.query.userID;
 console.log(userID);
 var profileRef = db.ref(`/Users/${userID}`);
 
- profileRef.on('value', function(snapshot) {
+profileRef.on('value', function(snapshot) {
    res.send(snapshot.val());
  }, function (errorObject) {
    res.send("The read failed: " + errorObject.code);
  });
-
-
+/*
+client.query("select \"Users\".\"firstName\", \"Users\".\"lastName\", \"Users\".\"email\" from carpool.\"Users\" where \"Users\".\"userID\" = 'OWVqu8jLXLSjizorl9FkgfUtoiq2'")
+  .then(result => res.send(result.rows))
+  .catch(e => res.send(e.stack))
+/*/
 
 });
 
