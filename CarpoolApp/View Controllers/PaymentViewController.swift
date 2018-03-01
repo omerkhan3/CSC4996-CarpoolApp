9fc6e2d941e59792e4146103cd6e9c672c208c34
Muamer67 <m_besic@yahoo.com>
2018-03-01 03:58:57 -0500

    Created start of UI for payment view, needs way to retrieve recent payments in table view and add new payment method clicking button

---
+++ CarpoolApp/View Controllers/PaymentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PaymentViewController.swift
+//  CarpoolApp
+//
+//  Created by muamer besic on 3/1/18.
+//  Copyright © 2018 CSC 4996. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+    
+    @IBOutlet weak var paymentsTable: UITableView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func newPayment(_ sender: Any) {
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
