c7bebfaa2981107647172bd3719194547883f132
Muamer67 <m_besic@yahoo.com>
2018-03-02 17:39:29 -0500

    Able to add new payment method by clicking 'add new payment method' button and made some UI updates

--- CarpoolApp/View Controllers/PaymentsViewController.swift
+++ CarpoolApp/View Controllers/PaymentsViewController.swift
@@ -7,38 +7,87 @@
 //
 
 import UIKit
+import Braintree
+import BraintreeDropIn
+import Firebase
+import FirebaseDatabase
+import FirebaseCore
+import FirebaseAuth
 
 class PaymentsViewController: UIViewController {
 
+    //Tokenization key needed to authenticate with Braintree Sandbox. Since it is just a Sandbox account we have hard-coded the key in but for production this key would need to be hosted elsewhere
+    let tokenizationKey =  "sandbox_vtqbvdrz_kjjqnn2gj7vbds9g"
     
+    //When button is clicked, Drop-In is initialized
     @IBAction func newPaymentMethod(_ sender: Any) {
+        showDropIn(clientTokenOrTokenizationKey: self.tokenizationKey)
     }
-    
-    
-    
-    
-    
-    
-    
-    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Shows the braintree Drop-In UI
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request = BTDropInRequest()
+        let dropIn = BTDropInController(authorization: self.tokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("Error.")
+            }
+            else if (result?.isCancelled == true) {
+                print("Cancelled.")
+            }
+            else if let paymentResult = result {
+                let selectedPaymentMethod = paymentResult.paymentMethod?.nonce
+                self.postNonceToServer(paymentMethodNonce: selectedPaymentMethod!)
+            }
+            controller.dismiss(animated: true, completion: nil)
+            }
+        self.present(dropIn!, animated: true, completion: nil)
+        }
+    
+    //Method to send unique payment nonce to server for transaction
+    func postNonceToServer(paymentMethodNonce: String) {
+        let currentUser = Auth.auth().currentUser
+        currentUser?.getIDTokenForcingRefresh(true) { idToken, error in
+            if let error = error {
+                print (error.localizedDescription)
+                return;
+            }
+            let userToken = idToken!
+            //URL endpoint of our local node server
+            let paymentURL = URL(string: "http://localhost:3000/payment/")!
+            var request = URLRequest(url: paymentURL)
+            //payment_method_nonce is the field that the server will be looking for to receive the nonce
+            request.httpBody = "payment_method_nonce=\(paymentMethodNonce)&idToken=\(userToken)".data(using: String.Encoding.utf8)
+            //POST method
+            request.httpMethod = "POST"
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+                //Error handling response
+                if (error != nil){
+                    print ("An error has occured.")
+                }
+                else{
+                    print ("Success!")
+                }
+                }.resume()
+        }
     }
 }
 
 //Need this new class when you are repeating content in a table view cell, error when no new class
 class TableViewCell: UITableViewCell {
-    
     static let reuseIdentifier = "TableViewCell"
-    
     @IBOutlet weak var contactLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
