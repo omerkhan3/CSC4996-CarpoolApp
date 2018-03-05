503e26b4e3688cff3df3838bd3287cdfa9aa5148
Omer  Khan <okhan320@gmail.com>
2018-03-04 20:57:54 -0500

    Matching Query added.

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -13,13 +13,14 @@ const pgp = db.$config.pgp;
 
 router.get('/', function(req, res, next) {
 var userID = req.query.userID;
-db.one("select \"Users\".\"firstName\", \"Users\".\"lastName\", \"Users\".\"Email\" from carpool.\"Users\" where \"Users\".\"userID\" = $1", userID)
+var matchesQuery = " Select \"driverFirstName\", \"driverLastName\", \"driverBiography\", \"driverID\", \"driverEndPointLat\", \"driverEndPointLong\", \"driverRouteUserID\", \"driverArrival\", \"driverDeparture\", \"driverRouteName\", \"driverRouteID\", \"matchID\", \"riderID\"  FROM (select \"firstName\" as \"driverFirstName\", \"lastName\" as \"driverLastName\", \"Biography\" as \"driverBiography\", \"userID\" from carpool.\"Users\") g JOIN  ((select \"driverID\" as \"driverID2\", \"userID\" as \"driverUserID\" from carpool.\"Drivers\") f JOIN ((Select \"endPointLat\" as \"driverEndPointLat\", \"endPointLong\" as \"driverEndPointLong\", \"driverID\" as \"driverRouteUserID\", \"Days\" as \"driverDays\", \"arrivalTime\" as \"driverArrival\", \"departureTime\" as \"driverDeparture\", \"Name\" as \"driverRouteName\", \"routeID\" as \"driverRouteID2\" FROM carpool.\"driverRoute\") c JOIN ((Select * FROM carpool.\"Matches\") a JOIN (Select \"userID\", \"riderID\" as \"riderID2\" FROM carpool.\"Riders\" where \"userID\" = $1) b ON a.\"riderID\" = b.\"riderID2\") d ON c.\"driverRouteUserID\" = d.\"driverID\") e  ON e.\"driverID\" = f.\"driverID2\") h ON g.\"userID\" = h.\"driverUserID\"";
+db.query(matchesQuery, userID)
 .then(function(data) {
   console.log(data);
   res.status(200).json({
     status: 'Success',
     data: data,
-    message:  'Retrieved User Profile.'
+    message:  'Retrieved User Matches.'
   });
 })
   .catch(function(err){
