1c66e08b51bf3760e8b2b2040614596f93402a6e
Omer  Khan <okhan320@gmail.com>
2018-03-05 23:48:29 -0500

    Fixed radius query to 3 miles.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -56,26 +56,25 @@ router.post('/', function(req, res, next) {
  }
 
  else {
- var matchingQuery = "SELECT * FROM carpool.\"Routes\" WHERE ST_DWithin(startPoint, Geography(ST_MakePoint($1, $2)),1000) AND ST_DWithin(endPoint, Geography(ST_MakePoint($3, $4)),1000) AND (\"departureTime\" <= ($5) AND \"departureTime\" >= ($5  - interval '15 minutes')) AND (\"arrivalTime\" <= ($6 + interval '15 minutes') AND \"arrivalTime\" >= ($6)) AND \"Matched\" = 'false' AND \"Driver\" = 'true'";
+ var matchingQuery = "SELECT * FROM carpool.\"Routes\" WHERE ST_DWithin(startPoint, Geography(ST_MakePoint($1, $2)),4830) AND ST_DWithin(endPoint, Geography(ST_MakePoint($3, $4)),4830) AND (\"departureTime\" <= ($5) AND \"departureTime\" >= ($5  - interval '15 minutes')) AND (\"arrivalTime\" <= ($6 + interval '15 minutes') AND \"arrivalTime\" >= ($6)) AND \"Matched\" = 'false' AND \"Driver\" = 'true'";
      db.any(matchingQuery, [routeJSON['Latitudes'][0], routeJSON['Longitudes'][0], routeJSON['Latitudes'][1], routeJSON['Longitudes'][1], convertTo24Hour(routeJSON['departureTime']), convertTo24Hour(routeJSON['arrivalTime'])])
      .then(function(result) {
        if (result.length > 0){
-            //console.log('Match Found: ', data);
+            //console.log('Match Found: ', result);
             for (var key in result) {
-              // skip loop if the property is from prototype
               if (!result.hasOwnProperty(key)) continue;
               var obj = result[key];
               db.any("INSERT INTO carpool.\"Matches\"(\"riderID\", \"driverID\",  \"driverRouteID\", \"Status\") values($1, $2, $3, $4)",[userID, obj['driverID'], obj['routeID'], "Awaiting rider request."  ] )
 
 }
-            db.any("INSERT INTO carpool.\"notificationLog\"(\"userID\", \"notificationType\", \"Date\", \"Read\") values ($1, $2, $3, $4)", [userID, "Match", 'now', 'false']);
+           db.any("INSERT INTO carpool.\"notificationLog\"(\"userID\", \"notificationType\", \"Date\", \"Read\") values ($1, $2, $3, $4)", [userID, "Match", 'now', 'false']);
           }
        else {
           console.log("No match found.");
        }
      });
      var addRiderRouteQuery = "INSERT INTO carpool.\"Routes\"(\"riderID\", \"departureTime\", \"arrivalTime\", \"startPointLong\", \"startPointLat\", \"endPointLong\", \"endPointLat\", \"Name\", \"Days\", \"Matched\", \"Driver\") values($1, $2, $3, $4, $5, $6, $7, $8, $9, 'false', 'false')";
-     db.any(addRiderRouteQuery, [
+    db.any(addRiderRouteQuery, [
       userID, routeJSON['departureTime'], routeJSON['arrivalTime'], routeJSON['Longitudes'][0], routeJSON['Latitudes'][0], routeJSON['Longitudes'][1], routeJSON['Latitudes'][1], routeJSON['Name'], routeJSON['Days']])
       .then(function () {
         console.log("Succesfully added route.");
@@ -90,9 +89,12 @@ router.post('/', function(req, res, next) {
             message: 'Rider route stored.'
           });
       })
+
       .catch(function (err) {
         console.log('error adding route:', err);
       });
+
+
  }
 
 });
