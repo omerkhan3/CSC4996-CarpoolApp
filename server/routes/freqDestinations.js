eedb69a91081b3156acd2c1a86e6ffab9a38a9f8
Muamer67 <m_besic@yahoo.com>
2018-03-13 16:50:13 -0400

    Added js code for post and get methods for frequent destinations and my routes view

---
+++ server/routes/freqDestinations.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+var admin = require('firebase-admin');
+var router = express.Router();
+const db = require('../routes/db');
+const pgp = db.$config.pgp;
+
+router.post('/frequentDestinations', function(req, res, next) {
+	var routeInfo = req.body.routeInfo;
+	var routeJSON = JSON.parse(routeInfo);
+	var userID = routeJSON['userID'];
+	
+	db.one("INSERT INTO carpool.\"Users\"(\"userID\", \"homeAddress\", \"schoolAddress\", \"workAddress\") values($1, $2, $3, $4)", [
+	userID, routeJSON['homeAddress'], routeJSON['schoolAddress'], routeJSON['workAddress']])
+	.then(function() {
+		res.status(200)
+			.json({
+				status: 'Success',
+				message: "Route info Stored'
+			});
+		})
+		.catch(function(err) {
+			res.send(err);
+		});
+	});
+
+
+router.post('/frequentDestinations', function(req, res, next) {
+	var routeInfo = req.body.routeInfo;
+	var routeJSON = JSON.parse(routeInfo);
+	console.log(routeJSON);
+	var userID = routeJSON['userID'];
+	var home = routeJSON['homeAddress'];
+	var school = routeJSON['schoolAddress'];
+	var work = routeJSON['workAddress'];
+	console.log("Updating Frequent Destinations.");
+	db.query("UPDATE carpool.\"Users\" SET \"homeAddress\" = $1, \"schoolAddress\" = $2, \"workAddress\" = $3 where \"userID\" = $4", [home, school, work, userID])
+	.then(function() {
+		res.status(200)
+			.json({
+				status: 'Success'
+				message: 'User Routes Updated.'
+			});
+		})
+		.catch(function(err) {
+			res.send(err);
+		});
+	});
+	
+module.exports = router;
+	
---
+++ server/routes/myRoutes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var firebase = require('firebase');
+var admin = require('firebase-admin');
+var router = express.Router();
+const db = require('../routes/db');
+const pgp = db.$config.pgp;
+
+
+router.get('/frequentDestinations', function(req, res, next) {
+var userID = req.query.userID;
+console.log(userID);
+db.one("select \"Users\".\"homeAddress\",\"Users\".\"schoolAddress\",\"Users\".\"workAddress\" from carpool.\"Users\" where \"Users\".\"userID\" = $1", userID)
+.then(function(data) {
+	console.log(data);
+	res.status(200).json({
+		status: 'Success',
+		data: data,
+		message: 'Retrieved Route Info.'
+	});
+});
+	.catch(function(err) {
+		console.log(err);
+	})
+});
+
+module.exports = router;
