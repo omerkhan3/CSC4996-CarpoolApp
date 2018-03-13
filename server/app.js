e4650a067ded08acccb8d0c2f9e8fbb7d05f9a34
Muamer67 <m_besic@yahoo.com>
2018-03-13 18:10:32 -0400

    Added js code for get method to freqdestinations.js, got rid of myroutes.js and added code in app.js pertaining to freqdestinations

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ var payment = require('./routes/payment');
 var routes = require('./routes/routes');
 var notifications = require('./routes/notifications');
 var matches = require('./routes/matches');
+var freqDestinations = require('./routes/freqDestinations');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -37,6 +38,7 @@ app.use('/payment', payment);  // using the checkout route we are using to handl
 app.use('/routes', routes);
 app.use('/notifications', notifications);
 app.use ('/matches', matches);
+app.use('/freqDestinations', freqDestinations);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- server/routes/freqDestinations.js
+++ server/routes/freqDestinations.js
@@ -27,7 +27,23 @@ router.post('/frequentDestinations', function(req, res, next) {
 			res.send(err);
 		});
 	});
-
+	
+router.get('/frequentDestinations', function(req, res, next) {
+	var userID = req.query.userID;
+	console.log(userID);
+	db.one("select \"Users\".\"homeAddress\",\"Users\".\"schoolAddress\",\"Users\".\"workAddress\",\"Users\".\"CustomAddress\",\"Users\".\"Custom\" from carpool.\"Users\" where \"Users\".\"userID\" = $1", userID)
+	.then(function(data) {
+		console.log(data);
+		res.status(200).json({
+			status: 'Success',
+			data: data,
+			message: 'Retrieved Route Info.'
+		});
+	});
+		.catch(function(err) {
+			console.log(err);
+		})
+	});
 
 router.post('/frequentDestinations', function(req, res, next) {
 	var routeInfo = req.body.routeInfo;
