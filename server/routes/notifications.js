ac3efdb1c1e7faad9cae9cb6a273361842fe7da4
Omer  Khan <okhan320@gmail.com>
2018-03-14 16:24:36 -0400

    Push Notification POC Working

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -8,6 +10,45 @@ var admin = require('firebase-admin');
 var router = express.Router();
 const db = require('../routes/db');
 const pgp = db.$config.pgp;
+
+
+const apn = require('apn');
+
+let options = {
+   token: {
+     key: "cert_notifications.p8",
+    // Replace keyID and teamID with the values you've previously saved.
+     keyId: "3KL4446N4D",
+     teamId: "3Z83W2KF98"
+  },
+   production: false
+ };
+
+ let apnProvider = new apn.Provider(options);
+
+ // Replace deviceToken with your particular token:
+ let deviceToken = "2E008C45EBC2562C1C0F0467294C10C794D11CBB98F4CDF5D2C9185BF33A8C3F";
+
+// Prepare the notifications
+let notification = new apn.Notification();
+ notification.expiry = Math.floor(Date.now() / 1000) + 24 * 3600; // will expire in 24 hours from now
+notification.badge = 2;
+notification.sound = "ping.aiff";
+notification.alert = "Hello from Omer";
+notification.payload = {'messageFrom': 'Notifications are working!'};
+
+ // Replace this with your app bundle ID:
+ notification.topic = "com.CSC4996.CarpoolApp";
+
+ // Send the actual notification
+apnProvider.send(notification, deviceToken).then( result => {
+// Show the result of the send operation:
+ 	console.log(result);
+});
+
+
+ // Close the server
+apnProvider.shutdown();
 
 
 
