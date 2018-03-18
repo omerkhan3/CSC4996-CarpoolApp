0519b6ac91b4379b40a8fa82a3a62f6d95ce3c20
Muamer67 <m_besic@yahoo.com>
2018-03-17 22:10:00 -0400

    Added code for getting recent payments started

--- CarpoolApp/View Controllers/PaymentsViewController.swift
+++ CarpoolApp/View Controllers/PaymentsViewController.swift
@@ -19,21 +19,97 @@ class PaymentsViewController: UIViewController {
     //let tokenizationKey =  "sandbox_vtqbvdrz_kjjqnn2gj7vbds9g"
     let clientToken = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJhYTBlYzBjMzI3MjEzM2UzYTQ5ZmFiMDJjNzljYzc5YjBmM2Y5NTI2NTI4OTQ0ODFkMTM3N2E4NWNlNGU0YTdkfGNyZWF0ZWRfYXQ9MjAxOC0wMy0xM1QwNDo1ODo1My42MjAzMTExMzUrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tLzM0OHBrOWNnZjNiZ3l3MmIifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0="
     
+    //array of payments
+    var recentPaymentsArray = [RecentPayments]()
+    var recentPayments = RecentPayments()
     @IBAction func newPaymentMethod(_ sender: Any) {
         showDropIn(clientTokenOrTokenizationKey: self.clientToken)
     }
     
     @IBOutlet weak var tableView: UITableView!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getPayments {
+            self.tableView.reloadData()
+            if self.recentPaymentsArray.count == 0 {
+                // No payments alert
+                let actionTitle = "Uh oh.."
+                let actionItem = "You have no payments!"
+                
+                // Activate UIAlertController to display confirmation
+                let alert = UIAlertController(title: actionTitle, message: actionItem, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        recentPayments = recentPaymentsArray[indexPath.row]
+        print(recentPayments)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableView(style: UITableViewCellStyle.subtitle, reuseIdentifier: "TableViewCell")
+        
+        return cell
+    }
+    
+    // set number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // set number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recentPaymentsArray.count
+    }
+    
+    // Download payments JSON and decode into an array
+    func getPayments(completed: @escaping () -> ()) {
+        // get userID
+        let userID = Auth.auth().currentUser?.uid
+        var viewMatchComponents = URLComponents(string: "http://localhost:3000/payment/Payments")!
+        viewMatchComponents.queryItems = [URLQueryItem(name: "userID", value: userID)]
+        var request = URLRequest(url: viewMatchComponents.url!)  // Pass Parameter in URL
+        print (viewMatchComponents.url!)
+        
+        request.httpMethod = "GET" // GET METHOD
+        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+            if (error != nil){  // error handling responses.
+                print (error as Any)
+            } else {
+                guard let data = data else { return }
+                do {
+                    // decode JSON into recentPayments[] array type
+                    self.recentPaymentsArray = try JSONDecoder().decode([RecentPayments].self, from: data)
+                    print(self.recentPaymentsArray)
+                    DispatchQueue.main.async {
+                        completed()
+                    }
+                } catch let jsnErr {
+                    print(jsnErr)
+                }
+            }
+            }.resume()
+    }
+    
     
     func fetchClientToken() {
         let clientTokenURL = URL(string: "http://localhost:3000/payment/")!
