918c024c55c5586c6ca75772df23513a4c424795
Omer  Khan <okhan320@gmail.com>
2018-03-19 19:55:11 -0400

    Fixed bug with adding routes.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 var admin = require('firebase-admin');
 var index = require('./routes/index');
 
@@ -20,6 +21,9 @@ var matches = require('./routes/matches');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/payment', payment);  // using the checkout route we are using to handle nonce.
