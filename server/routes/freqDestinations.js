3b50bab076bb5287cb014274b197939732d54418
Muamer67 <m_besic@yahoo.com>
2018-03-21 00:32:03 -0400

    Edited get methods on js side, currently gives 500 error message

--- server/routes/freqDestinations.js
+++ server/routes/freqDestinations.js
@@ -10,71 +10,121 @@ const db = require('../routes/db');
 const pgp = db.$config.pgp;
 
 
-
 router.get('/homeDestination', function(req, res, next) {
-	var userID = req.query.userID;
-	console.log(userID);
-	db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
-	.then(function(data) {
-		console.log(data);
-		res.status(200).json({
-			status: 'Success',
-			data: data,
-			message: 'Retrieved Home Info.'
-		});
-	})
+	//var userID = req.query.userID;
+	var homeInfo = req.body.homeInfo;
+	var homeJSON = JSON.parse(homeInfo);
+	console.log(homeInfo);
+	//console.log(userID);
+	var homeLengthJSON = homeJSON.length;
+	console.log("Home JSON", homeLengthJSON);
+	
+	for (var l = 0; l < homeLengthJSON; l++)
+	{
+		var name = homeJSON[l]['Name'];
+		var address = homeJSON[l]['Address'];
+		console.log("Element", l, name, homeJSON[l]['Address']);
+		db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
+		.then(function(data) {
+			console.log(data);
+			res.status(200).json({
+				status: 'Success',
+				data: data,
+				message: 'Retrieved Home Info.'
+			});
+		})
 		.catch(function(err) {
 			console.log(err);
 		})
-	});
+	};
+});
+	
 router.get('/schoolDestination', function(req, res, next) {
-	var userID = req.query.userID;
-	console.log(userID);
-	db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
-	.then(function(data) {
-		console.log(data);
-		res.status(200).json({
-			status: 'Success',
-			data: data,
-			message: 'Retrieved Home Info.'
-		});
-	})
+	//var userID = req.query.userID;
+	var schoolInfo = req.body.schoolInfo;
+	var schoolJSON = JSON.parse(schoolInfo);
+	console.log(schoolInfo);
+	//console.log(userID);
+	var schoolLengthJSON = schoolJSON.length;
+	console.log("School JSON", schoolLengthJSON);
+	
+	for (var l = 0; l < schoolLengthJSON; l++)
+	{
+		var name = schoolJSON[l]['Name'];
+		var address = schoolJSON[l]['Address'];
+		console.log("Element", l, name, schoolJSON[l]['Address']);
+		db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
+		.then(function(data) {
+			console.log(data);
+			res.status(200).json({
+				status: 'Success',
+				data: data,
+				message: 'Retrieved Home Info.'
+			});
+		})
 		.catch(function(err) {
 			console.log(err);
 		})
-	});
+	};
+});
+	
 router.get('/workDestination', function(req, res, next) {
-	var userID = req.query.userID;
-	console.log(userID);
-	db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
-	.then(function(data) {
-		console.log(data);
-		res.status(200).json({
-			status: 'Success',
-			data: data,
-			message: 'Retrieved Home Info.'
-		});
-	})
+	//var userID = req.query.userID;
+	var workInfo = req.body.workInfo;
+	var workJSON = JSON.parse(workInfo);
+	console.log(workInfo);
+	//console.log(userID);
+	var workLengthJSON = workJSON.length;
+	console.log("Work JSON", workLengthJSON);
+	
+	for (var l = 0; l < workLengthJSON; l++)
+	{
+		var name = workJSON[l]['Name'];
+		var address = workJSON[l]['Address'];
+		console.log("Element", l, name, workJSON[l]['Address']);
+		db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
+		.then(function(data) {
+			console.log(data);
+			res.status(200).json({
+				status: 'Success',
+				data: data,
+				message: 'Retrieved Home Info.'
+			});
+		})
 		.catch(function(err) {
 			console.log(err);
 		})
-	});
+	};
+});
+	
 router.get('/customDestination', function(req, res, next) {
-	var userID = req.query.userID;
-	console.log(userID);
-	db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
-	.then(function(data) {
-		console.log(data);
-		res.status(200).json({
-			status: 'Success',
-			data: data,
-			message: 'Retrieved Home Info.'
-		});
-	})
+	//var userID = req.query.userID;
+	var customInfo = req.body.customInfo;
+	var customJSON = JSON.parse(customInfo);
+	console.log(customInfo);
+	//console.log(userID);
+	var customLengthJSON = customJSON.length;
+	console.log("Custom JSON", customLengthJSON);
+	
+	for (var l = 0; l < customLengthJSON; l++)
+	{
+		var name = customJSON[l]['Name'];
+		var address = customJSON[l]['Address'];
+		console.log("Element", l, name, customJSON[l]['Address']);
+		db.query("select \"frequentDestinations\".\"Address\" from carpool.\"frequentDestinations\" where \"frequentDestinations\".\"userID\" = $1", userID)
+		.then(function(data) {
+			console.log(data);
+			res.status(200).json({
+				status: 'Success',
+				data: data,
+				message: 'Retrieved Home Info.'
+			});
+		})
 		.catch(function(err) {
 			console.log(err);
 		})
-	});
+	};
+});
 
 
 router.post('/saveDestination', function(req, res, next) {
