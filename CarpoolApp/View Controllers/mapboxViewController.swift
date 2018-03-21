7fd17f583ec874f59383c2abe1b66d85762de33f
Matt <du4714@wayne.edu>
2018-03-21 16:04:22 -0400

    working on mapbox

---
+++ CarpoolApp/View Controllers/mapboxViewController.swift
@@ -0,0 +1,35 @@
+//
+//  mapboxViewController.swift
+//  CarpoolApp
+//
+//  Created by Matt on 3/21/18.
+//  Copyright © 2018 CSC 4996. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class mapboxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
