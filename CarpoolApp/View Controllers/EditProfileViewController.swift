3ce6f103dd61dcfa446bdaebea26e8c47cdaffd3
Muamer67 <m_besic@yahoo.com>
2018-03-24 21:42:30 -0400

    Start of phone number formatting code

--- CarpoolApp/View Controllers/EditProfileViewController.swift
+++ CarpoolApp/View Controllers/EditProfileViewController.swift
@@ -217,6 +217,56 @@ class EditProfileViewController: UIViewController, UITextFieldDelegate, UIImageP
                 }.resume()
         }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        if textField == PhoneField
+        {
+            let newString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+            let components = newString.components(separatedBy: NSCharacterSet.decimalDigits)
+            
+            let decimalStrin = ""
+            let decimalString = components.joined(separator: decimalStrin) as NSString
+            let length = decimalString.length
+            let hasLeadingOne = length > 0 && decimalString.character(at: 0) == (1 as unichar)
+
+            if length == 0 || (length > 10 && !hasLeadingOne) || length > 11
+            {
+                let newLength = (textField.text! as NSString).length + (string as NSString).length - range.length as Int
+                
+                return (newLength > 10) ? false : true
+            }
+            var index = 0 as Int
+            let formattedString = NSMutableString()
+            
+            if hasLeadingOne
+            {
+                formattedString.append("1 ")
+                index += 1
+            }
+            if (length - index) > 3
+            {
+                let areaCode = decimalString.substring(with: NSMakeRange(index, 3))
+                formattedString.appendFormat("(%@)", areaCode)
+                index += 3
+            }
+            if length - index > 3
+            {
+                let prefix = decimalString.substring(with: NSMakeRange(index, 3))
+                formattedString.appendFormat("%@-", prefix)
+                index += 3
+            }
+            
+            let remainder = decimalString.substring(from: index)
+            formattedString.append(remainder)
+            PhoneField.text = formattedString as String
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
