f4a010f2e468b08cc96f3f77df8146f6de4e6e42
Matt <du4714@wayne.edu>
2018-03-25 22:38:24 -0400

    added button controls and error is cancel while in the middle of route

--- CarpoolApp/View Controllers/EmbeddedNavViewController.swift
+++ CarpoolApp/View Controllers/EmbeddedNavViewController.swift
@@ -3,16 +3,52 @@ import MapboxCoreNavigation
 import MapboxNavigation
 import MapboxDirections
 
-class EmbeddedNavViewController: UIViewController, NavigationViewControllerDelegate  {
+class EmbeddedNavViewController: UIViewController, NavigationViewControllerDelegate, UITextFieldDelegate  {
  
     @IBOutlet weak var container: UIView!
     var route: Route?
 
+    @IBOutlet weak var pickedUp: RoundedButton!
+    @IBOutlet weak var droppedOff: RoundedButton!
+    @IBOutlet weak var cancelDrive: RoundedButton!
+    
     let origin = Waypoint(coordinate: CLLocationCoordinate2DMake(42.382184, -82.940201), name: "matts place")
   let destination = Waypoint(coordinate: CLLocationCoordinate2DMake(42.359139, -83.066546), name: "wayne state")
     let riderPickup = Waypoint(coordinate: CLLocationCoordinate2DMake(42.3840825, -82.9412279), name: "1320 Lakepointe")
     let riderDropoff = Waypoint(coordinate: CLLocationCoordinate2DMake(42.3863712, -82.942725), name:"1420 Lakepointe")
         
+    @IBAction func pickPress(_ sender: RoundedButton) {
+        //cancelDrive.isEnabled = false
+        cancelDrive.alpha = 0.2
+    }
+    
+    @IBAction func dropPress(_ sender: RoundedButton) {
+        //cancelDrive.isEnabled = true
+        cancelDrive.alpha = 1.0
+     
+    }
+    
+    @IBAction func cancelPress(_ sender: RoundedButton) {
+        
+        var actionItem : String=String()
+        var actionTitle : String=String()
+        let exitAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        // default action to exit out of native alerts.
+        if cancelDrive.alpha != 1.0 {
+            actionTitle = "Error!"
+            actionItem = "You can not cancel ride in the middle of a route."
+            
+            // Activate UIAlertController to display error
+            let alert = UIAlertController(title: actionTitle, message: actionItem, preferredStyle: .alert)
+            alert.addAction(exitAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if cancelDrive.alpha == 1.0 {
+            print("hi")
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +86,9 @@ class EmbeddedNavViewController: UIViewController, NavigationViewControllerDeleg
         self.didMove(toParentViewController: self)
     }
     func navigationViewController(_ navigationViewController: NavigationViewController, didArriveAt waypoint: Waypoint) -> Bool {
+        
+        
+        
         let alert = UIAlertController(title: "Arrived at \(String(describing: waypoint.name))", message: "Would you like to continue?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
             // Begin the next leg once the driver confirms
