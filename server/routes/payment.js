6c6c1a733139fd3145c095266888a5b004d8b5cc
Muamer67 <m_besic@yahoo.com>
2018-04-05 03:10:02 -0400

    Updates to payments.js

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -12,14 +12,6 @@ var gateway = braintree.connect({
     privateKey:   '06195f7e1a9007cc81e2ad48899682da'
 });
 
-router.get("/client_token",function (req, res) {
-  gateway.clientToken.generate({
-  customerId: "224286744"
-  }, function (err, result) {
-  res.send(result.clientToken)
-  });
-});
-
 router.get("/recentPayments", function (req, res, next) {
 var userID = req.query.userID;
 db.query(`select \"userID\", \"Time\", \"riderID\", \"driverID\", \"rideCost\" from carpool.\"paymentHistory\",\"Matches\"`)
@@ -28,8 +20,26 @@ db.query(`select \"userID\", \"Time\", \"riderID\", \"driverID\", \"rideCost\" f
   });
 });
 
+
+router.get("/client_token",function (req, res) {
+  gateway.clientToken.generate({
+  customerId: "customerToken"
+  }, function (err, result) {
+  res.send(result.clientToken)
+  });
+});
+
+
 router.post("/checkout", function (req, res) {
   var userID = req.query.userID;
+  
+  var ScheduledRide = req.body.ScheduledRide;
+  var ScheduledRideJSON = JSON.parse(ScheduledRide);
+  //var Amount = ScheduledRideJSON["rideCost"]
+  
+  var RecentPayments = req.body.RecentPayments;
+  var RecentPaymentsJSON = JSON.parse(RecentPayments);
+  
   // Use the payment method nonce here
   var nonceFromTheClient = req.body.payment_method_nonce;
   db.query(`select \"userID\", \"customerToken\" from carpool.\"Users\"`)
@@ -37,20 +47,22 @@ router.post("/checkout", function (req, res) {
         amount: "10.00",  // we have hardcoded $10 for now for all transactions, we will eventually need to add "amount" field to our HTTP message from client.
         paymentMethodNonce: nonceFromTheClient,  // we use the nonce received from client.
         customer: {
-        	id: "224286744"
+        	id: "customerToken"
         },
         options: {
-          storeInVaultOnSuccess: true
-          //submitForSettlement: true  // this commad is what tells Braintree to process the transaction.
+          failOnDuplicatePaymentMethod: true,
+          storeInVaultOnSuccess: true,
+          submitForSettlement: true  // this command is what tells Braintree to process the transaction.
         }
       }, function(err, result) {
           if (result.success) {
-            db.query(`INSERT INTO carpool.\"paymentHistory\"(\"userID\", \"Time\") VALUES ('${userID}', '${Time}') where \"Time\" >= 'now'`)
+            //db.query(`INSERT INTO carpool.\"paymentHistory\"(\"userID\", \"Amount\", \"Time\") VALUES ('${userID}', '${Time}', '${Amount}') where \"Time\" >= 'now', \"Amount\" = [Amount]`)
+            db.query(`INSERT INTO carpool.\"paymentHistory\"(\"userID\", \"Amount\", \"Time\") values ($1, $2, $3)", [userID, ScheduledRideJSON['rideCost'], RecentPaymentsJSON['Time']])
             result.success;
             result.transaction.type;
             result.transaction.status;
           } else {
-          console.log(err);
+            console.log(err);
             res.status(500).send(err);
           }
       });
