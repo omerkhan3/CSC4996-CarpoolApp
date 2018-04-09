f0743252bfd3a86602fa804d9a82d883c48b5a9f
Matt <du4714@wayne.edu>
2018-04-09 14:06:17 -0400

    added terms view controller

---
+++ CarpoolApp/View Controllers/TermsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpViewController.swift
+//  CarpoolApp
+//
+//  Created by Matt on 4/4/18.
+//  Copyright © 2018 CSC 4996. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+ 
+    
+}
+}
