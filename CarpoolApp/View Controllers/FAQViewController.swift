6f6f97e2959737107c84c1c8a9271306633ff0bc
Matt <du4714@wayne.edu>
2018-04-10 04:16:01 -0400

    added a few faqs pdf, went through some constraints

--- CarpoolApp/View Controllers/FAQViewController.swift
+++ CarpoolApp/View Controllers/FAQViewController.swift
@@ -7,29 +7,20 @@
 //
 
 import UIKit
+import WebKit
 
 class FAQViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "FAQSs", ofType: "pdf")
+        let url = URL(fileURLWithPath: path!)
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
--- CarpoolApp/View Controllers/TermsViewController.swift
+++ CarpoolApp/View Controllers/TermsViewController.swift
@@ -19,23 +19,7 @@ class TermsViewController: UIViewController {
         let request = URLRequest(url: url)
         
         webView.load(request)
-        // Do any additional setup after loading the view.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
