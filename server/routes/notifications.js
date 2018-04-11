6cbf0d61f880e2efae61a41ee97921aff1cbdc5d
Omer  Khan <okhan320@gmail.com>
2018-04-10 22:01:55 -0400

    Notifications query now ordered by date

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -2,14 +2,14 @@
 var router = express.Router();
 
 const db = require('../routes/db'); // configures connection to the DB.
-const pgp = db.$config.pgp; 
+const pgp = db.$config.pgp;
 
 
 // This GET request sends all notifications associated with the passed userID.
 router.get('/', function(req, res, next) {
 var userID = req.query.userID;
 console.log(userID);
-db.query(`select \"notificationLog\".\"notificationType\", \"notificationLog\".\"Date\", \"notificationLog\".\"Read\" from carpool.\"notificationLog\" where \"notificationLog\".\"userID\" = '${userID}'`) // Query to view all notifications associated with a particular user.
+db.query(`select \"notificationLog\".\"notificationType\", \"notificationLog\".\"Date\", \"notificationLog\".\"Read\" from carpool.\"notificationLog\" where \"notificationLog\".\"userID\" = '${userID}' ORDER BY \"Date\" desc`) // Query to view all notifications associated with a particular user.
 .then(function(data) {
  console.log("Data:" , data);
  res.send(data);
